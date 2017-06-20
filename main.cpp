08bb1450020744eb2ab76d0d6478738e8d3724fd
Michele Vascellari <Michele.Vascellari@vtc.tu-freiberg.de>
2017-06-20 16:00:42 +0200

    odeint works with pointer

--- main.cpp
+++ main.cpp
@@ -17,14 +17,11 @@ namespace pl = std::placeholders;
 
 void printv(const dvector &y, const double t);
 
-int main()
+int main2()
 {
   SFOR model;
-  //boost::numeric::odeint::integrate_const(std::bind(), y0, t0, tEnd, dt, printv);
-  //SFOR model;
-  //std::cout << "Model name=" << model.getName() << std::endl;
-  //delete model;
 
+  // variables
   double t=0;
   double tend = 0.1;
   double dt=1e-4;
@@ -41,16 +38,38 @@ int main()
 
   std::cout << "[bind] dydt=" << dydt[0] << std::endl;
 
-  
-  //boost::numeric::odeint::integrate(std::bind(&SFOR::calcRate, model, pl::_1 , pl::_2 , pl::_3),
-  //				    y , t , tend,  dt);
   boost::numeric::odeint::integrate(fct,
   				    y , t , tend,  dt, printv);
-  
+  return 0;
 }
 
 
 void printv(const dvector &y, const double t)
 {
   std::cout << "t=" << t << " y=" << y[0] << std::endl;
+}
+
+
+int main()
+{
+  Model *model = new SFOR();
+
+  // variables
+  double t=0;
+  double tend = 0.1;
+  double dt=1e-4;
+  dvector y = {0, 1000};
+  dvector dydt(2);
+
+  model->calcRate(y, dydt, t);
+  std::cout << "dydt=" << dydt[0] << std::endl;
+  auto fct = std::bind(&Model::calcRate, model, pl::_1 , pl::_2 , pl::_3);
+
+  fct(y, dydt, t);
+
+  std::cout << "[bind] dydt=" << dydt[0] << std::endl;
+  
+  boost::numeric::odeint::integrate(fct,
+  				    y , t , tend,  dt, printv);
+  return 0;
 }
