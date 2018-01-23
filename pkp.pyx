fb44ae9e3e26fe88e87970bc60175d38b8762f13
Michele Vascellari <Michele.Vascellari@vtc.tu-freiberg.de>
2018-01-23 20:10:43 +0100

    Extend cython module to operating_conditions

--- pkp.pyx
+++ pkp.pyx
@@ -5,13 +5,16 @@ from libcpp cimport bool
 cdef extern from "reactor.hpp" namespace "pkp":
     cdef cppclass CxxReactor "pkp::Reactor":
         CxxReactor(string, vector[double])
-        void solve(double t, double T, double dt, bool verbose)
+        void solve(double dt, bool verbose)
         vector[double] getParameters()
         void setParameters(vector[double])
         vector[double] getTimes()
         vector[vector[double]] getStates()
         void dump(string csv, string sep)
         void printParameters()
+        vector[vector[double]] getOperatingConditions()
+        void setOperatingConditions(vector[vector[double]])
+
 
 
 cdef class Reactor:
@@ -21,12 +24,10 @@ cdef class Reactor:
         cdef vector[double] c_parameters = parameters
         self._c_reactor = new CxxReactor(c_name, c_parameters)
 
-    def solve(self, t, T, dt, verbose=True):
-        cdef double t_c = t
-        cdef double T_c = T
+    def solve(self, dt, verbose=True):
         cdef double dt_c = dt
         cdef bool verbose_c = verbose
-        self._c_reactor.solve(t_c, T_c, dt_c, verbose_c)
+        self._c_reactor.solve(dt_c, verbose_c)
 
     def _get_parameters(self):
         return self._c_reactor.getParameters()
@@ -53,3 +54,12 @@ cdef class Reactor:
         cdef string csv_c = csv.encode()
         cdef sep_c = sep.encode()
         self._c_reactor.dump(csv_c, sep_c)
+
+    def _get_operating_conditions(self):
+        return self._c_reactor.getOperatingConditions()
+
+    def _set_operating_conditions(self, oc):
+        cdef vector[vector[double]] oc_c = oc
+        self._c_reactor.setOperatingConditions(oc_c)
+
+    operating_conditions = property(_get_operating_conditions, _set_operating_conditions)
--- test_pkp.py
+++ test_pkp.py
@@ -1,6 +1,7 @@
 import pkp
 import numpy as np
 import matplotlib.pyplot as plt
+from dualplot import DualPlot
 
 reactor = pkp.Reactor('SFOR', [1e6, 50e6, 0.5])
 
@@ -10,13 +11,22 @@ reactor.parameters = [1e5, 45e6, 0.45]
 
 print(reactor.parameters)
 
-reactor.solve(0.01, 1200, 1e-6, verbose=False)
+oc = [[0, 500], [0.1, 1000], [0.2, 1000]]
+reactor.operating_conditions = oc
+
+reactor.solve(1e-6, verbose=False)
 
 times = np.array(reactor.times)
 states = np.array(reactor.states)
 
 reactor.dump('state.csv')
 
-plt.plot(times, states[:, 0], marker='x')
+# plt.plot(times, states[:, 0], marker='x')
+dp = DualPlot()
+dp.plot_left(times, states[:, 0], marker='x')
+dp.plot_right(times, states[:, 1])
+dp.set_xlabel('Times, s')
+dp.set_ylabel_left('Yields')
+dp.set_ylabel_right('Temperature, K')
 plt.show()
 # reactor._set_parameters()
